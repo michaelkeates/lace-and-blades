06f99f7ca00df6adb7262252d053d4f488aafe6b
michaelkeates <mail@michaelkeates.co.uk>
2026-02-21 17:06:45 +0000

    update footer font

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,15 @@
 import { Box } from '@chakra-ui/react'
-//import 'rc-footer/assets/index.css'; // import 'rc-footer/asssets/index.less';
 
 const Footer = () => {
   return (
-    <Box align="center" opacity={0.4} fontSize="11px" paddingTop="25px" paddingBottom="35px" fontFamily="roboto">
+    <Box
+      align="center"
+      opacity={0.4}
+      fontSize="11px"
+      paddingTop="25px"
+      paddingBottom="35px"
+      fontFamily="footer"
+    >
       &copy; {new Date().getFullYear()} Michael Keates. All Rights Reserved.
     </Box>
   )
