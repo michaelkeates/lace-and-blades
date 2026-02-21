74605a37a8dcedb2b6740132722efb73ad78613a
michaelkeates <mail@michaelkeates.co.uk>
2026-02-21 14:05:01 +0000

    Update fonts.js

--- components/fonts.js
+++ components/fonts.js
@@ -1,6 +1,7 @@
 const Fonts = () => (
   <style jsx global>{`
     @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap');
+    @import url('ttps://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&family=Cinzel:wght@400..900&display=swap');
   `}</style>
 )
 export default Fonts
