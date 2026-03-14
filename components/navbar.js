1c3d3fb2e88de6fb4b8dfc7ebc2c93ddb7be3cbd
michaelkeates <mail@michaelkeates.co.uk>
2026-03-14 14:52:33 +0000

    Donation page updates for styling

--- components/navbar.js
+++ components/navbar.js
@@ -26,7 +26,8 @@ import {
   LuPhone,
   LuShoppingBag,
   LuFileHeart,
-  LuPersonStanding
+  LuPersonStanding,
+  LuDoorClosed
 } from 'react-icons/lu'
 
 import Logo from './logo'
@@ -34,76 +35,68 @@ import ThemeToggleButton from './buttons/theme-toggle-button'
 import TikTokButton from './buttons/tiktok-button'
 import LinkedinButton from './buttons/linkedin-button'
 import LinktrButton from './buttons/linktr-button'
-import WordpressButton from './buttons/wordpress-button'
 
 const allMenuItems = [
-  {
-    label: "Georgia's Law",
-    icon: <LuCastle size={18} />,
-    path: '/georgias-law',
-    type: 'icon'
-  },
-  {
-    label: 'Support & Helplines',
-    icon: <LuPhone size={18} />,
-    path: '/support-helplines',
-    type: 'icon'
-  },
+  { label: "Georgia's Law", icon: <LuCastle size={18} />, path: '/georgias-law', type: 'icon' },
+  { label: 'Support & Helplines', icon: <LuPhone size={18} />, path: '/support-helplines', type: 'icon' },
   { label: 'Blog', icon: <LuBook size={18} />, path: '/posts', type: 'icon' },
-  {
-    label: 'Support Agencies Information',
-    icon: <LuBook size={18} />,
-    path: '/get-information-agencies',
-    type: 'icon'
-  },
-  {
-    label: 'Giving Back - Donations & Fundraisers',
-    icon: <LuFileHeart size={18} />,
-    path: '/giving-back-donations-fundraisers',
-    type: 'icon'
-  },
-  {
-    label: 'Speaking & Testimony',
-    icon: <LuPersonStanding size={18} />,
-    path: '/speaking-testimony',
-    type: 'icon'
-  },
-  {
-    label: 'Shop - Buy the Book',
-    icon: <LuShoppingBag size={18} />,
-    path: '/shop',
-    type: 'icon'
-  },
-  {
-    label: "Questions we don't want to answer",
-    icon: <LuBook size={18} />,
-    path: '/questions-we-dont-want-to-answer',
-    type: 'icon'
-  },
-  {
-    label: 'Campaign News & Action',
-    icon: <LuBook size={18} />,
-    path: '/media-press',
-    type: 'icon'
-  },
-  {
-    label: 'Terms, Transparency, Privacy & Affiliations',
-    icon: <LuBook size={18} />,
-    path: '/terms-transparency-privacy-affiliations',
-    type: 'icon'
-  }
+  { label: 'Support Agencies Information', icon: <LuBook size={18} />, path: '/get-information-agencies', type: 'icon' },
+  { label: 'Giving Back - Donations & Fundraisers', icon: <LuFileHeart size={18} />, path: '/giving-back-donations-fundraisers', type: 'icon' },
+  { label: 'Speaking & Testimony', icon: <LuPersonStanding size={18} />, path: '/speaking-testimony', type: 'icon' },
+  { label: 'Shop - Buy the Book', icon: <LuShoppingBag size={18} />, path: '/shop', type: 'icon' },
+  { label: "Questions we don't want to answer", icon: <LuBook size={18} />, path: '/questions-we-dont-want-to-answer', type: 'icon' },
+  { label: 'Campaign News & Action', icon: <LuBook size={18} />, path: '/media-press', type: 'icon' },
+  { label: 'Terms, Transparency, Privacy & Affiliations', icon: <LuBook size={18} />, path: '/terms-transparency-privacy-affiliations', type: 'icon' },
+  { label: 'Admin Login', icon: <LuDoorClosed size={18} />, path: 'https://laceandblades.michaelkeates.co.uk/wp-login.php', type: 'icon', external: true }
 ]
 
 const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const menuBg = useColorModeValue(
-    'rgba(206,158,224,0.95)',
-    'rgba(36,31,39,0.95)'
-  )
-  const menuHover = useColorModeValue(
-    'rgba(141,84,180,0.9)',
-    'rgba(81,43,113,0.9)'
-  )
+  const menuBg = useColorModeValue('rgba(206,158,224,0.95)', 'rgba(36,31,39,0.95)')
+  const menuHover = useColorModeValue('rgba(141,84,180,0.9)', 'rgba(81,43,113,0.9)')
+
+  // Reusable function for rendering a menu item
+  const renderMenuItem = (item) => {
+    if (item.external) {
+      return (
+        <Link
+          key={item.label}
+          href={item.path}
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={onClose}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          p={2}
+          borderRadius="md"
+          _hover={{ bg: 'whiteAlpha.200' }}
+        >
+          {item.icon && <Box mb={1}>{item.icon}</Box>}
+          <Box fontSize="xs" textAlign="center">{item.label}</Box>
+        </Link>
+      )
+    } else {
+      return (
+        <NextLink key={item.label} href={item.path} passHref legacyBehavior>
+          <Flex
+            as={Link}
+            direction="column"
+            align="center"
+            cursor="pointer"
+            onClick={onClose}
+            p={2}
+            borderRadius="md"
+            _hover={{ bg: 'whiteAlpha.200' }}
+          >
+            {item.icon && <Box mb={1}>{item.icon}</Box>}
+            <Box fontSize="xs" textAlign="center">{item.label}</Box>
+          </Flex>
+        </NextLink>
+      )
+    }
+  }
 
   return (
     <Box
@@ -114,13 +107,7 @@ const Navbar = () => {
       style={{ backdropFilter: 'blur(10px)' }}
       zIndex="999"
     >
-      <Container
-        display="flex"
-        p={2}
-        maxW="container.md"
-        align="center"
-        justify="space-between"
-      >
+      <Container display="flex" p={2} maxW="container.md" align="center" justify="space-between">
         {/* Logo */}
         <Flex align="center">
           <Heading as="h1" size="lg">
@@ -129,11 +116,7 @@ const Navbar = () => {
         </Flex>
 
         {/* Desktop Menu */}
-        <Stack
-          direction="row"
-          display={{ base: 'none', md: 'flex' }}
-          align="center"
-        >
+        <Stack direction="row" display={{ base: 'none', md: 'flex' }} align="center">
           <Menu isOpen={isOpen} onClose={onClose}>
             <MenuButton
               as={Button}
@@ -143,91 +126,30 @@ const Navbar = () => {
             >
               Menu
             </MenuButton>
-            <MenuList
-              bg={menuBg}
-              style={{ backdropFilter: 'blur(10px)' }}
-              maxW="400px"
-              minW="300px"
-              w="fit-content"
-            >
+            <MenuList bg={menuBg} style={{ backdropFilter: 'blur(10px)' }} maxW="400px" minW="300px" w="fit-content">
               {/* Icon Grid */}
               <Flex mb={2} px={2}>
                 <Box
                   display="grid"
-                  gridTemplateColumns="repeat(4, 1fr)" // 4 icons per row
-                  gap={1} // spacing between icons
+                  gridTemplateColumns="repeat(4, 1fr)"
+                  gap={1}
                   w="100%"
-                  justifyItems="center" // center icons horizontally in each cell
+                  justifyItems="center"
                 >
-                  {allMenuItems
-                    .filter(item => item.type === 'icon')
-                    .map(item => (
-                      <NextLink
-                        key={item.label}
-                        href={item.path}
-                        passHref
-                        legacyBehavior
-                      >
-                        <Flex
-                          as={Link}
-                          direction="column"
-                          align="center"
-                          cursor="pointer"
-                          _hover={{ bg: 'whiteAlpha.200' }}
-                          p={2}
-                          borderRadius="md"
-                          onClick={onClose}
-                        >
-                          {item.icon}
-                          <Box fontSize="xs" mt={1}>
-                            {item.label}
-                          </Box>
-                        </Flex>
-                      </NextLink>
-                    ))}
+                  {allMenuItems.filter(item => item.type === 'icon').map(renderMenuItem)}
                 </Box>
               </Flex>
-
-              {/* Text Links */}
-              {allMenuItems
-                .filter(item => item.type === 'text')
-                .map(item => (
-                  <NextLink
-                    key={item.label}
-                    href={item.path}
-                    passHref
-                    legacyBehavior
-                  >
-                    <MenuItem
-                      as={Link}
-                      _hover={{ bg: menuHover }}
-                      onClick={onClose}
-                    >
-                      {item.label}
-                    </MenuItem>
-                  </NextLink>
-                ))}
             </MenuList>
           </Menu>
         </Stack>
 
         {/* Right side social & theme buttons */}
         <Flex flex={1} ml={1} justify="flex-end" align="center">
-          <Box ml={2}>
-            <TikTokButton />
-          </Box>
-          <Box ml={2}>
-            <LinkedinButton />
-          </Box>
-          <Box ml={2}>
-            <LinktrButton />
-          </Box>
-          <Box ml={2}>
-            <WordpressButton />
-          </Box>
-          <Box ml={2}>
-            <ThemeToggleButton />
-          </Box>
+          <Box ml={2}><TikTokButton /></Box>
+          <Box ml={2}><LinkedinButton /></Box>
+          <Box ml={2}><LinktrButton /></Box>
+          <Box ml={2}><ThemeToggleButton /></Box>
+
           {/* Mobile Menu */}
           <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
             <Menu isLazy>
@@ -238,42 +160,9 @@ const Navbar = () => {
                 aria-label="Options"
                 _focus={{ boxShadow: 'none' }}
               />
-              <MenuList
-                bg={menuBg}
-                css={{ backdropFilter: 'blur(10px)' }}
-                maxW="400px"
-                w="fit-content"
-              >
-                <Box
-                  display="grid"
-                  gridTemplateColumns="repeat(2, 1fr)"
-                  gap={2}
-                  p={2}
-                  justifyItems="center"
-                >
-                  {allMenuItems.map(item => (
-                    <MenuItem
-                      key={item.label}
-                      as={NextLink}
-                      href={item.path}
-                      onClick={onClose}
-                      display="flex"
-                      flexDirection="column"
-                      alignItems="center"
-                      justifyContent="center"
-                      p={2}
-                      borderRadius="md"
-                      bg="transparent"
-                      _hover={{ bg: 'whiteAlpha.200' }}
-                      _focus={{ bg: 'whiteAlpha.200' }}
-                      _active={{ bg: 'whiteAlpha.200' }}
-                    >
-                      {item.icon && <Box mb={1}>{item.icon}</Box>}
-                      <Box fontSize="xs" textAlign="center">
-                        {item.label}
-                      </Box>
-                    </MenuItem>
-                  ))}
+              <MenuList bg={menuBg} css={{ backdropFilter: 'blur(10px)' }} maxW="400px" w="fit-content">
+                <Box display="grid" gridTemplateColumns="repeat(2, 1fr)" gap={2} p={2} justifyItems="center">
+                  {allMenuItems.map(renderMenuItem)}
                 </Box>
               </MenuList>
             </Menu>
--- pages/giving-back-donations-fundraisers.js
+++ pages/giving-back-donations-fundraisers.js
@@ -1,35 +1,243 @@
-// pages/georgias-law.js
 import { getApolloClient } from '../lib/wordpress'
-import { Container, Button } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  SimpleGrid,
+  Link,
+  Button,
+  useColorModeValue
+} from '@chakra-ui/react'
 import styles from '../styles/Home.module.css'
 import Section from '../components/section'
+import parse, { domToReact } from 'html-react-parser'
 import { GET_GIVING_BACK_PAGE } from '../lib/queries'
-import NextLink from 'next/link'
 
 export default function GivingBackDonationsFundraisers({ page }) {
   if (!page) return <p>Page not found</p>
 
-  return (
-    <layout>
-      <Container maxW="4xl" mt="-7rem">
-        <Section delay={0.1}>
-          <main className={styles.main}>
-            <h1>{page.title}</h1>
+  const renderedPDFs = new Set()
+  const pdfs = []
 
-            {page.featuredImage && (
-              <img
-                className={styles.featuredImage}
-                src={page.featuredImage.node.sourceUrl}
-                alt={page.title}
-              />
-            )}
-            <div>
-              <div dangerouslySetInnerHTML={{ __html: page.content }} />
-            </div>
-          </main>
-        </Section>
-      </Container>
-    </layout>
+  // Replace nodes recursively
+  function replaceNode(node) {
+    if (!node) return
+
+    // Columns container
+    if (
+      node.name === 'div' &&
+      node.attribs?.class?.includes('wp-block-columns')
+    ) {
+      return (
+        <Box
+          className="wp-block-columns"
+          display="flex"
+          flexWrap="wrap"
+          gap="1rem"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          width="100%"
+        >
+          {domToReact(node.children, { replace: replaceNode })}
+        </Box>
+      )
+    }
+
+    // Individual column
+    if (
+      node.name === 'div' &&
+      node.attribs?.class?.includes('wp-block-column')
+    ) {
+      return (
+        <Box
+          className="wp-block-column"
+          flex="1 1 48%"
+          minWidth="200px"
+          mb={4}
+          boxSizing="border-box"
+        >
+          {domToReact(node.children, { replace: replaceNode })}
+        </Box>
+      )
+    }
+    8
+
+    // Figure with image
+    if (node.name === 'figure') {
+      return (
+        <Box as="figure" mb={4}>
+          {domToReact(node.children, {
+            replace: child => {
+              if (child.name === 'img') {
+                return (
+                  <img
+                    {...child.attribs}
+                    style={{ width: '100%', height: 'auto', display: 'block' }}
+                  />
+                )
+              }
+              if (child.name === 'figcaption') {
+                return (
+                  <Box as="figcaption" textAlign="center" fontSize="sm" mt={1}>
+                    {domToReact(child.children)}
+                  </Box>
+                )
+              }
+              return replaceNode(child)
+            }
+          })}
+        </Box>
+      )
+    }
+
+    // PDFs
+    if (
+      node.name === 'a' &&
+      node.attribs?.href?.toLowerCase().endsWith('.pdf')
+    ) {
+      const href = node.attribs.href
+      if (!renderedPDFs.has(href)) {
+        renderedPDFs.add(href)
+        const title = node.children?.[0]?.data || 'PDF Document'
+        pdfs.push({ href, title })
+      }
+      return <></>
+    }
+
+    if (
+      node.name === 'object' &&
+      node.attribs?.data?.toLowerCase().endsWith('.pdf')
+    ) {
+      const href = node.attribs.data
+      if (!renderedPDFs.has(href)) {
+        renderedPDFs.add(href)
+        const title = node.attribs['aria-label'] || 'PDF Document'
+        pdfs.push({ href, title })
+      }
+      return <></>
+    }
+
+    // Paragraphs
+    if (node.name === 'p')
+      return (
+        <Box mb={3} fontSize="lg">
+          {domToReact(node.children, { replace: replaceNode })}
+        </Box>
+      )
+
+    // Headings
+    if (node.name === 'h1')
+      return (
+        <Box as="h1" fontSize="4xl" my={4}>
+          {domToReact(node.children, { replace: replaceNode })}
+        </Box>
+      )
+    if (node.name === 'h2')
+      return (
+        <Box
+          as="h2"
+          fontSize="3xl"
+          my={6}
+          textAlign={
+            node.attribs?.class?.includes('has-text-align-center')
+              ? 'center'
+              : 'left'
+          }
+        >
+          {domToReact(node.children, { replace: replaceNode })}
+        </Box>
+      )
+    if (node.name === 'h3')
+      return (
+        <Box as="h3" fontSize="2xl" my={4}>
+          {domToReact(node.children, { replace: replaceNode })}
+        </Box>
+      )
+
+    // Links
+    if (node.name === 'a')
+      return (
+        <Link href={node.attribs.href} isExternal color="blue.500">
+          {domToReact(node.children, { replace: replaceNode })}
+        </Link>
+      )
+
+    // Lists
+    if (node.name === 'ul')
+      return (
+        <Box as="ul" mb={3}>
+          {domToReact(node.children, { replace: replaceNode })}
+        </Box>
+      )
+    if (node.name === 'li')
+      return (
+        <Box as="li">{domToReact(node.children, { replace: replaceNode })}</Box>
+      )
+  }
+
+  const content = parse(page.content, { replace: replaceNode })
+
+  return (
+    <Container maxW="4xl" mt="-7rem">
+      <Section delay={0.1}>
+        <main className={styles.main}>
+          <h1>{page.title}</h1>
+
+          {page.featuredImage && (
+            <img
+              className={styles.featuredImage}
+              src={page.featuredImage.node.sourceUrl}
+              alt={page.title}
+            />
+          )}
+
+          {/* WordPress content */}
+          <Box mb={10} className={`${styles.wpContent} wp-content-wrapper`}>
+            {content}
+          </Box>
+
+          {/* PDFs below content */}
+{pdfs.length > 0 && (
+  <SimpleGrid columns={{ base: 1, md: 2 }} spacing={6} mb={10} mt={6}>
+    {pdfs.map(pdf => (
+      <Box
+        key={pdf.href}
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        p={3}
+      >
+        <embed
+          src={pdf.href}
+          type="application/pdf"
+          width="100%"
+          height="300px"
+        />
+        <Box mt={2} display="flex" justifyContent="center">
+          <Link
+            href={pdf.href}
+            isExternal
+            width="100%"
+            _hover={{ textDecoration: 'none' }}
+          >
+            <Button
+              width="100%" // Button fills column width
+              bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+              _hover={{ bg: useColorModeValue('whiteAlpha.600', 'whiteAlpha.300') }}
+              whiteSpace="normal" // Allow text to wrap
+              textAlign="center"
+              py={4}
+            >
+              {pdf.title}
+            </Button>
+          </Link>
+        </Box>
+      </Box>
+    ))}
+  </SimpleGrid>
+)}
+        </main>
+      </Section>
+    </Container>
   )
 }
 
