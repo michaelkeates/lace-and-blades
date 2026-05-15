8fec4d1e58c6c177f794cc0ea5bfb530a54ab82d
michaelkeates <mail@michaelkeates.co.uk>
2026-05-15 08:43:28 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ['laceandblades.michaelkeates.co.uk','avatars.githubusercontent.com','repository-images.githubusercontent.com', 'opengraph.githubassets.com'],
+    domains: ['wordpress.laceandblades.co.uk','avatars.githubusercontent.com','repository-images.githubusercontent.com', 'opengraph.githubassets.com'],
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
